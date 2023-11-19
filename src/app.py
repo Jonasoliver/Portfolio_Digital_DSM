a7b7116bed430e9dd184c5fb467879c22bdfffb1
Jonasoliver <142221848+Jonasoliver@users.noreply.github.com>
2023-11-19 16:05:00 -0300

    adicionei no arquivo app.py a opção de insesir uma lista na parte de trabalhos
    adicionei ao arquivo app.py a opção de lista do python, para adicionar formatação nas coisas

--- src/app.py
+++ src/app.py
@@ -1,18 +1,32 @@
-from flask import Flask
-from flask  import render_template  
+from flask import Flask, render_template
 
-app = Flask('__name__')
+app = Flask(__name__)
+
+# Lista de trabalhos
+trabalhos = [
+    {
+        'titulo': 'Projeto API',
+        'descricao': 'O projeto realizado visa ajudar aos pais de crianças que sofrem de doença renal crônica, ajudando na informatização, como por exemplo onde há hospitais no Brasil especializados na área, quais os cuidados devem ser tomados com crianças assim, uma área de conversas, troca de mensagens entre os pais, para tirar possíveis dúvidas.',
+        'imagem': '../static/api.jpeg'
+    },
+    {
+        'titulo': 'Projeto da Arte Desenvolvimento Web',
+        'descricao': 'Este projeto foi para a aprendizagem, visando a utilização do bootstrap e a capacidade de desenvolver uma arte própria em seu site, referindo-me ao front end.',
+        'imagem': '../static/arte.jpeg'
+    },
+   
+]
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/trabalhos')
-def quem():
-    return render_template('trabalhos.html')
+def mostrar_trabalhos():
+    return render_template('trabalhos.html', trabalhos=trabalhos)
 
 @app.route('/sobremim')
-def contato():
+def sobre_mim():
     return render_template('sobremim.html')
 
 if __name__ == '__main__':
